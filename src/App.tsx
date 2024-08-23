050a5b69ac3f5c57a17d931df9e67cd3d845ab41
AnemoneTK <Copp.teenkung@gmail.com>
2024-08-23 14:05:33 +0700

    [SETUP]-setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles/App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className=" bg-background w-dvw h-dvh overflow-hidden flex gap-0 md:gap-[20px] lg:gap-[40px] p-0 md:p-[35px] lg:px-[70px]">
+        <div className="canvas w-full border-0 md:border-2  bg-white rounded-xl p-6">
+          <div className=""></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      background: "#F6F4F5",
+      primary: "#14121F",
+      white: "#FFFF",
+      green: "#4CAF50",
+      yellow: "#FFC107",
+      gray: "#1F1F1F",
+    },
+  },
+  plugins: [],
+};
